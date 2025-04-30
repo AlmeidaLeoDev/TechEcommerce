88dd6cf4ade04e90062fda72cd6143b581975764
Leonardo <almeidaleo.dev@gmail.com>
2025-04-30 00:35:05 -0300

    chore(backend/config): remove EF6 module, comment connection string, refactor program

--- Back-End/TechEcommerce/TechEcommerce/Program.cs
+++ Back-End/TechEcommerce/TechEcommerce/Program.cs
@@ -5,13 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Connection String
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 3))));
 
 var app = builder.Build();
 
@@ -29,3 +28,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+Console.WriteLine($"ConnectionString: {connectionString}");
